ac3edbb56cec44e8274d70e5171c0679e4e7ece9
Idowu <idowuadigun2000@gmail.com>
2026-04-01 16:24:04 +0100

    Student routes completed

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 import studentRoutes from "./routes/students.js";
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+app.use(express.json());
 
 app.use("/api/students", studentRoutes);
 
--- routes/students.js
+++ routes/students.js
@@ -8,4 +8,17 @@ router.get("/", (req, res) => {
     .json({ message: "All Students -> studentController.getAllStudents" });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  return res.status(200).json({
+    message: `Find Students -> studentController.findStudent with id: ${id}`,
+  });
+});
+
+router.post("/", (req, res) => {
+  return res.status(201).json({
+    message: "Create Students -> studentController.createNewStudents",
+  });
+});
+
 export default router;
